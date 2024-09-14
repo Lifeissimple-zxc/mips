3af1c56113c7af3262fb7fde46be0030446f5960
Lifeissimple-zxc <mararkarp@gmail.com>
2024-09-14 21:31:52 +0200

    bad change removal

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
                     log.info("stopping loop because env is %s",
                             setup.app_config.env)
                     break
-                # log_cleaner.clean_log_files(path="./logs", delete=True)
+                log_cleaner.clean_log_files(path="./logs", delete=True)
                 to_sleep = round(setup.app_config.sleep_between_runs - custom_timer.elapsed, 2)  # noqa: E501
                 log.info("sleeping for %s minutes before next iteration",
                         round(to_sleep/60, 2))
